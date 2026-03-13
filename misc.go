a8c244764b5284c39838157a3575e36703e2bcdf
sameeroak1110 <sameeroak1110@gmail.com>
2026-03-13 12:41:09 +0530

    Misc helper functions.

---
+++ misc.go
@@ -0,0 +1,16 @@
+package helper
+
+import (
+	"math/rand"
+)
+
+func RandomInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano()) // seeding to nanoseconds granularity.
+
+	if min >= max {
+		return -1
+	}
+
+	//return rand.Intn(max - min) + min     // max is omitted. max limit is min through (max - 1.0)
+	return rand.Intn(max - min + 1) + min   // max is honoured. max limit is min through max.
+}
